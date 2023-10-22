50b9c9c0454b3ce2fc7eed4d771340639cadd010
polyx <polyx@DESKTOP-UF9B3R8>
2023-10-22 23:36:12 +0200

    Add project files.

---
+++ praca domowa/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Text;
+
+
+namespace praca_domowa
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+       
+
+
+    }   
+  
+    
+
+        
+        
+    
+    
+}
---
+++ praca domowa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+namespace paracadomowa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = 50;
+            double b = 30;
+
+            Math math = new Math();
+
+            double addResoult = math.Add(a, b);
+
+            double substractResoult = math.Substract(a, b);
+
+            Console.WriteLine(addResoult .ToString());
+            Console.WriteLine(substractResoult . ToString());
+            
+
+        }
+
+
+    }
+}
+
+
+
